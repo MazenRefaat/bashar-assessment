77e0ea4b831d287534cfafb95152037879f724fa
Mazen <Mazen@Mazen.local>
2020-01-30 21:49:51 +0200

    Sort Books by Name

--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper, Header, Title, Content, CardWrapper } from './Styles';
 import Card from '../Card/Card';
@@ -10,25 +10,28 @@ import Card from '../Card/Card';
 * @param {object} props props data to be rendered in list
 * @returns ListView component
 */
-const ListView = (props) => (
-    <Wrapper>
-        <Header>
-            <Title>
-                {props.title}
-            </Title>
-        </Header>
+const ListView = (props) => {
+    
+    return (
+        <Wrapper>
+            <Header>
+                <Title>
+                    {props.title}
+                </Title>
+            </Header>
 
-        <Content>
-            {
-                props.data.map((item, key)=> (
-                    <CardWrapper key={key}>
-                        <Card type='book' item={item} key={key}/>
-                    </CardWrapper>
-                ))
-            }
-        </Content>
-    </Wrapper>
-)
+            <Content>
+                {
+                    props.data.map((item, key)=> (
+                        <CardWrapper key={key}>
+                            <Card type='book' item={item} key={key}/>
+                        </CardWrapper>
+                    ))
+                }
+            </Content>
+        </Wrapper>
+    )
+}
 
 
 ListView.propTypes = {
--- src/views/Home/Styles.js
+++ src/views/Home/Styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+export const Label = styled.label`
+    display: block;
+    margin-bottom: 15px;
+`
+
 export const Wrapper = styled.section`
     display: flex;
     justify-content: space-between;
